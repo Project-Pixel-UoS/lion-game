f70bf8897033625fed440fa4ef8b8a9ef08aeaa8
ReyFern <rehan.davian@gmail.com>
2025-12-10 13:19:12 +0000

    [feat]: Add collision script between enemy and watering hole

---
+++ Assets/Scripts/EnemyCollisionScript.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System;
+
+public class EnemyCollisionScript : MonoBehaviour
+{
+    public GameObject enemy;
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Detects collision with enemy object
+    /// </summary>
+    /// <param name="other">collision object</param>
+    /// <todo>
+    /// Change from destruction of object to having the object run away after a few seconds
+    /// </todo>
+    /// <remarks>
+    /// Maintained by: Rehan Fernando
+    /// </remarks>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Returns 1 if watering hole position is less than enemy position and -1 if greater than
+    /// </summary>
+    /// <param name="wateringHolePosition">position of the watering hole on one axis</param>
+    /// <param name="enemyPosition">position of the enemy object on one axis</param>
+    /// <returns>-1 or 1 depending on where enemy is compared to watering hole</returns>
+    /// <remarks>
+    /// Maintained by: Rehan Fernando
+    /// </remarks>
+    public int Signum(float wateringHolePosition, float enemyPosition)
+    {
+        int result = 0;
+
+        if (wateringHolePosition < enemyPosition)
+        {
+            result = 1;
+        }
+        else if (wateringHolePosition > enemyPosition)
+        {
+            result = -1;
+        }
+
+        return result;
+    }
+}
