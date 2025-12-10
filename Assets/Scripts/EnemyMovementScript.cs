90480be197894b83c020aa199c464035f0f59290
ReyFern <rehan.davian@gmail.com>
2025-12-10 12:07:22 +0000

    [wip]: Enemies now move towards the watering hole object

--- Assets/Scripts/EnemyMovementScript.cs
+++ Assets/Scripts/EnemyMovementScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class EnemyMovementScript : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    public float speed = 20f;
+    public float speed;
     public GameObject wateringHole;
 
     private GameObject enemy;
@@ -12,6 +12,7 @@ public class EnemyMovementScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        speed = 1f;
         enemy = Instantiate(enemyPrefab, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);
         rb = enemy.GetComponent<Rigidbody2D>();
     }
@@ -19,6 +20,22 @@ public class EnemyMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (wateringHole.transform.position.x > enemy.transform.position.x)
+        {
+            rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+        }
+        else if (wateringHole.transform.position.x < enemy.transform.position.x)
+        {
+            rb.linearVelocity = new Vector2(-speed, rb.linearVelocity.y);
+        }
+
+        if (wateringHole.transform.position.y > enemy.transform.position.y)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, speed);
+        }
+        else if (wateringHole.transform.position.y < enemy.transform.position.y)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, -speed);
+        }
     }
 }
