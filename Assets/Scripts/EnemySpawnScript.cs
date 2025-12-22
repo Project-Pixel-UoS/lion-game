c5cd1858d31004b7bebe8463901f0a88376482b9
izzysmith06 <isabelle.amy.smith@icloud.com>
2025-12-22 14:38:06 +0000

    [feat] Add Enemy Spawn Script

---
+++ Assets/Scripts/EnemySpawnScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemySpawnScript : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public float minSpawnTime = 1f;
+    public float maxSpawnTime = 4f;
+
+    private float timer;
+    private float nextSpawnTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nextSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= nextSpawnTime)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+
+            timer = 0f;
+            nextSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        }
+    }
+}
