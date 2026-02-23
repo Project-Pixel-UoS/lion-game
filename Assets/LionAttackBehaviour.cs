cdeed64e59bc7bbc3b0b723229ba91eb035d1748
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2026-02-23 20:56:28 +0000

    wip: Add cone of raycasts to be able to detect enemies
    The cone can be adjusted in a few ways, such as the angle of spread, number of raycasts used, and the length - (Maybe this can be changed for the different types of lion?)

--- Assets/LionAttackBehaviour.cs
+++ Assets/LionAttackBehaviour.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
 
-public class LionAttackBehaviour : MonoBehaviour
+public class LionAttackBehaviour : MonoBehaviour 
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float maxDistance = 1f;
+    public float coneAngle = 60f;   // total angle
+    public int rayCount = 20;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CastCone();
+    }
+
+    void CastCone()
+    {
+        float startAngle = -coneAngle / 2f;
+        float angleStep = coneAngle / (rayCount - 1);
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            float currentAngle = startAngle + (angleStep * i);
+            Debug.Log("Current Angle: " + currentAngle);
+
+            // Rotate forward direction
+            Vector3 direction = Quaternion.Euler(0, 0, currentAngle) * transform.up;
+
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("Enemy detected!");
+                }
+            }
+
+            Debug.DrawRay(transform.position, direction * maxDistance, Color.red);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        float startAngle = -coneAngle / 2f;
+        float angleStep = coneAngle / (rayCount - 1);
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            float currentAngle = startAngle + (angleStep * i);
+
+            // Rotate forward direction
+            Vector3 direction = Quaternion.Euler(0, 0, currentAngle) * transform.up;
+            
+            Gizmos.color = new Color(1f, 1f, 0f);
+
+            // Draw the line
+            Gizmos.DrawLine(transform.position, transform.position + direction * maxDistance);
+        }
     }
 }
