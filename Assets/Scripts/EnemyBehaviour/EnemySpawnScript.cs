c38011f81931ef82ee78b9f81c67f7ecea7590ca
ReyFern <rehan.davian@gmail.com>
2026-03-10 23:32:20 +0000

    [refactor]: Rework enemy spawning so it's from the perspective of each lane not spawn point

--- Assets/Scripts/EnemyBehaviour/EnemySpawnScript.cs
+++ Assets/Scripts/EnemyBehaviour/EnemySpawnScript.cs
@@ -2,33 +2,31 @@ using UnityEngine;
 
 public class EnemySpawnScript : MonoBehaviour
 {
-
-    public GameObject enemyPrefab;
+    public GameObject[] enemyTypes = new GameObject[4];
     private float minSpawnTime = 8f;
     private float maxSpawnTime = 12f;
 
     private float timer;
     private float nextSpawnTime;
+    private GameObject[] spawnPoints = new GameObject[3];
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         nextSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        spawnPoints[0] = gameObject.transform.GetChild(0).gameObject;
+        spawnPoints[1] = gameObject.transform.GetChild(1).gameObject;
+        spawnPoints[2] = gameObject.transform.GetChild(2).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemyPrefab == null)
-        {
-            return;
-        }
-
         timer += Time.deltaTime;
 
         if (timer >= nextSpawnTime)
         {
-            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            Instantiate(enemyTypes[Random.Range(0, enemyTypes.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position, Quaternion.identity);
 
             timer = 0f;
             nextSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
