7c7d9c97406c1ecc17a7a7a7e6ca8bfe1a2c33df
ReyFern <rehan.davian@gmail.com>
2026-03-13 17:30:56 +0000

    [refactor]: Remove unnecessary method from collision script

--- Assets/Scripts/EnemyBehaviour/EnemyCollisionScript.cs
+++ Assets/Scripts/EnemyBehaviour/EnemyCollisionScript.cs
@@ -74,29 +74,4 @@ public class EnemyCollisionScript : MonoBehaviour
 
         enemy.SetActive(false);
     }
-
-    /// <summary>
-    /// Returns 1 if watering hole position is less than enemy position and -1 if greater than
-    /// </summary>
-    /// <param name="wateringHolePosition">position of the watering hole on one axis</param>
-    /// <param name="enemyPosition">position of the enemy object on one axis</param>
-    /// <returns>-1 or 1 depending on where enemy is compared to watering hole</returns>
-    /// <remarks>
-    /// Maintained by: Rehan Fernando
-    /// </remarks>
-    public int Signum(float wateringHolePosition, float enemyPosition)
-    {
-        int result = 0;
-
-        if (wateringHolePosition < enemyPosition)
-        {
-            result = 1;
-        }
-        else if (wateringHolePosition > enemyPosition)
-        {
-            result = -1;
-        }
-
-        return result;
-    }
 }
