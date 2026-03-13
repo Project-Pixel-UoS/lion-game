a5e8340c5b02e986405d7dbd21aa345058daa053
ReyFern <rehan.davian@gmail.com>
2026-03-13 17:17:11 +0000

    [chore]: Undo changes to enemy spawning

--- Assets/Scripts/EnemyBehaviour/EnemySpawnScript.cs
+++ Assets/Scripts/EnemyBehaviour/EnemySpawnScript.cs
@@ -2,34 +2,18 @@ using UnityEngine;
 
 public class EnemySpawnScript : MonoBehaviour
 {
-    public GameObject[] enemyTypes = new GameObject[5];
-    private float minSpawnTime = 8f;
-    private float maxSpawnTime = 12f;
+    public Direction group;
 
-    private float timer;
-    private float nextSpawnTime;
-    private GameObject[] spawnPoints = new GameObject[3];
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        nextSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
-        spawnPoints[0] = gameObject.transform.GetChild(0).gameObject;
-        spawnPoints[1] = gameObject.transform.GetChild(1).gameObject;
-        spawnPoints[2] = gameObject.transform.GetChild(2).gameObject;
+        WaveManager.Instance?.RegisterSpawnPoint(this);
     }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer += Time.deltaTime;
-
-        if (timer >= nextSpawnTime)
-        {
-            Instantiate(enemyTypes[Random.Range(0, enemyTypes.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position, Quaternion.identity);
-
-            timer = 0f;
-            nextSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
-        }
-    }
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West,
 }
