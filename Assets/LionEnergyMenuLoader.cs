e822c42da068ab85b5dd3049fddd96785a55f5c8
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2026-03-14 14:01:49 +0000

    wip: Create list for all lions and energy prefabs that can be spawned in with code

--- Assets/LionEnergyMenuLoader.cs
+++ Assets/LionEnergyMenuLoader.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class LionEnergyMenuLoader : MonoBehaviour
 {
+    public GameObject buttonPrefab; // Prefab for the buttons to spawn in the menu
+    public List<GameObject> lionPrefabs;
+    public List<GameObject> energyPrefabs;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SpawnButtons();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnButtons()
     {
-        
+        foreach (GameObject lion in lionPrefabs)
+        {
+            GameObject buttonObj = Instantiate(buttonPrefab, transform);
+            DeployLionButton button = buttonObj.GetComponent<DeployLionButton>();
+            button.lionPrefab = lion;
+        }
     }
 }
