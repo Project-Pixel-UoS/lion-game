01713b7e76aeb76539c4476d0f5f829ed13166bc
ReyFern <rehan.davian@gmail.com>
2026-03-18 12:55:12 +0000

    Create fruit tree prefab and behaviour script

---
+++ Assets/Scripts/FruitTreeBehaviour.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FruitTreeBehaviour : MonoBehaviour 
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float maxDistance = 1f;
+    public float coneAngle = 60f;   // total angle
+    public int rayCount = 20;
+
+    void Update()
+    {
+        
+    }
+}
